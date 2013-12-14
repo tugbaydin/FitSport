1cc1e669523a8dff4b70ea5e14992a5556729a1d
yunus <yunus@yunusmete.com>
2013-12-14 22:46:13 +0200

    CustomSpinner özelliği eklendi.

--- FitSport/src/edu/sdu/fitsport/AktiviteFragmentTab.java
+++ FitSport/src/edu/sdu/fitsport/AktiviteFragmentTab.java
@@ -4,16 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.Spinner;
 
 import com.actionbarsherlock.app.SherlockFragment;
+
+import edu.sdu.fitsport.adapter.CustomSpinnerAdapter;
 
 
 public class AktiviteFragmentTab extends SherlockFragment {
 
+	String[] spinnerDeger = {"Koþma", "Yüzme", "Basketbol"};
+	int[] spinnerResim = {R.drawable.running, R.drawable.swimming, R.drawable.basketball};
+	Spinner spSecim;
+	
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.aktivite_fragment_tab, container, false);
+		View view = inflater.inflate(R.layout.aktivite_fragment_tab, container, false);	
+		spSecim = (Spinner) view.findViewById(R.id.secim_spinner);
+		spSecim.setAdapter(new CustomSpinnerAdapter(getActivity(), R.layout.custom_spinner, spinnerDeger, spinnerResim));
+		
+		
 		return view;
 	}
+	
+	
 }
